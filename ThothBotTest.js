0d0608dfdfc21ce8629cbe833f1df05cc6d3606c
unbrace3 <unbrace3@gmail.com>
2017-02-04 21:12:10 -0800

    ThothBot Test Quizlet Connected!
    Properly authorizes Quizlet

--- ThothBotTest.js
+++ ThothBotTest.js
@@ -3,13 +3,32 @@
             context.console.log("test")
             if(isNewUser(context)) {
                 var state = new Date().getTime().toString();
+                context.simpledb.roomleveldata.state = state;
                 context.simpledb.doPut(state,event.contextobj);
-             }
+                var button = {
+                    "type": "survey",
+                    "question": "To begin, you need to link your Quizlet account to ThothBot. Please press authorize!",
+                    "msgid": "authorizebutton",
+                    "options": [{
+                    "type": "url",
+                    "title": "Authorize",
+                    "url": "quizlet.com/authorize?response_type=code&client_id=4Hxdpv7gBE&scope=write_set&state=" + state
+                }]
+                }
+                context.sendResponse(JSON.stringify(button));
+            }
+            else if (event.message.toLowerCase()=="kill") {
+                context.simpledb.roomleveldata.isnewuser = false;
+                context.sendResponse("hi");
+            } else if (event.message.toLowerCase()=="state") {
+                context.sendResponse(context.simpledb.roomleveldata.state)
+            }
+
        }
         function HttpEndpointHandler(context,event){
-            var dbkey = event.params.key
-            message = event.params.message;
-            context.simpledb.doPut(dbkey+"message", "{\"message\":\""+message+"\"}");
+            var dbkey = event.params.state
+            code = event.params.code;
+            context.simpledb.doPut(dbkey+"code", "{\"code\":\""+code+"\"}");
             context.simpledb.doGet(dbkey);
         }
 
