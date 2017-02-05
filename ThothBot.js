404090b76c98c6698e71807ac50300cf53ef006f
Alex Zhao <alexzhao21@gmail.com>
2017-02-05 06:12:04 -0800

    Add files via upload

---
+++ ThothBot.js
@@ -0,0 +1,112 @@
+var message = null;
+
+function MessageHandler(context, event) {
+  if (isNewUser(context)) {
+    var state = new Date().getTime().toString();
+    context.simpledb.roomleveldata.state = state;
+    context.simpledb.doPut(state, event.contextobj);
+    var button = {
+      "type": "survey",
+      "question": "To begin, you need to link your Quizlet account to ThothBot. Please press authorize!",
+      "msgid": "authorizebutton",
+      "options": [{
+        "type": "url",
+        "title": "Authorize",
+        "url": "quizlet.com/authorize?response_type=code&client_id=4Hxdpv7gBE&scope=read%20write_set&state=" +
+          state
+      }]
+    }
+  }
+  context.sendResponse(JSON.stringify(button));
+
+  if (event.messageobj.refmsgid == "authorizebutton" || event.message ==
+    "Main Menu" || (event.message == "Main Menu" && event.messageobj
+      .refmsgid == "failure")) {
+    var survey = {
+      "type": "survey",
+      "question": "Want to study?",
+      "msgid": "mainmenu",
+      "options": ["Learn", "Quick Quiz"]
+    }
+    context.sendResponse(JSON.stringify(survey));
+  }
+  if (event.message.toLowerCase() == "kill") {
+    context.simpledb.roomleveldata.isnewuser = false;
+    context.sendResponse("hi");
+  } else if (event.message.toLowerCase() == "state") {
+    context.sendResponse(context.simpledb.roomleveldata.state)
+  } else if (event.message.toLowerCase() == "listme") {
+    var lists = ["French", "Spanish", "Portugese"]
+    var arrayLength = lists.length;
+    var list = {
+      "type": "list",
+      "topElementStyle": "compact",
+      "msgid": "list123",
+      "items": [],
+    };
+    for (var i = 0; i < arrayLength; i++) {
+      var item = {
+        "title": lists[i],
+        "subtitle": "language",
+        "options": [{
+          "type": "text",
+          "title": "learn"
+        }]
+      }
+      list['items'].push(item);
+    }
+
+    context.sendResponse(JSON.stringify(list));
+    return;
+  } else { // otherwise go to main menu
+    var button = {
+      "type": "survey",
+      "question": "Not a valid command.",
+      "msgid": "failure",
+      "options": ["Main Menu"]
+    }
+    context.sendResponse(JSON.stringify(list));
+  }
+}
+
+function EventHandler(context, event) {
+  if (event.messageobj.refmsgid == "authorizebutton") {
+    context.sendResponse("followthrough");
+  }
+}
+
+function HttpEndpointHandler(context, event) {
+  var dbkey = event.params.state
+  code = event.params.code;
+  context.simpledb.doPut(dbkey + "code", "{\"code\":\"" + code + "\"}");
+  context.simpledb.doGet(dbkey);
+}
+
+function DbGetHandler(context, event) {
+  var url = "https://api.gupshup.io/sm/api/bot/ThothBotTest/msg"; //The bot which is sending the message to the user.
+  var header = {
+    "apikey": "3da26936384d4476c40c6eac52573b96",
+    "Content-Type": "application/x-www-form-urlencoded"
+  };
+  var contextobj = event.dbval;
+  var param = "context=" + contextobj + "&message=" + message;
+  context.simplehttp.makePost(url, param, header);
+}
+
+function DbPutHandler(context, event) {
+  context.sendResponse("Hello " + event.sender);
+}
+
+function HttpResponseHandler(context, event) {
+  context.sendResponse(event.getresp);
+}
+
+function isNewUser(context) {
+  var newuser = context.simpledb.roomleveldata.isnewuser;
+  if (!newuser) {
+    context.simpledb.roomleveldata.isnewuser = true;
+    return true;
+  } else {
+    return false;
+  }
+}
