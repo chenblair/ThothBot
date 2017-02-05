a2fea0709255cf8e837e28f1cf7eac2dc0eb606e
unbrace3 <unbrace3@gmail.com>
2017-02-04 18:30:23 -0800

    Add State
    oncreate makes a time-based state for a new user and stores it in the
    database

--- ThothBotTest.js
+++ ThothBotTest.js
@@ -1,10 +1,10 @@
  var message = null;
         function MessageHandler(context, event) {
             context.console.log("test")
-            if(event.message=='hi') {
-                context.simpledb.doPut(event.sender,event.contextobj);
+            if(isNewUser(context)) {
+                var state = new Date().getTime().toString();
+                context.simpledb.doPut(state,event.contextobj);
              }
-             context.sendResponse(event.sender);
        }
         function HttpEndpointHandler(context,event){
             var dbkey= event.params.key
@@ -15,7 +15,7 @@
 
         function DbGetHandler(context, event) {
             var url = "https://api.gupshup.io/sm/api/bot/ThothBotTest/msg";  //The bot which is sending the message to the user.
-            var header = {"apikey":"3da26936384d4476c40c6eac52573b96","Content-Type": "application/x-www-form-urlencoded"};  // you can get the API key from https://www.gupshup.io/developer/docs/bot-api/bot-api-ref
+            var header = {"apikey":"3da26936384d4476c40c6eac52573b96","Content-Type": "application/x-www-form-urlencoded"};
             var contextobj = event.dbval;
             var param = "context="+contextobj+"&message="+message;
              context.simplehttp.makePost(url,param,header);
@@ -27,4 +27,14 @@
 
         function HttpResponseHandler(context,event){
             context.sendResponse(event.getresp);
+        }
+        
+        function isNewUser(context) {
+            var newuser = context.simpledb.roomleveldata.isnewuser;
+            if (!newuser) {
+                context.simpledb.roomleveldata.isnewuser = true;
+                return true;
+            } else {
+                return false;
+            }
         }
